133dae9a98105cdc2d64b6ce744592fc5c88941a
Zed Fan <ff2110@nyu.edu>
2022-10-31 19:23:20 +0800

    wrap req and resp to context

--- route.go
+++ route.go
@@ -1,5 +1,10 @@
 package main
 
-func routeHandler() {
+import (
+	"coredemo/framework"
+)
 
+func registerRouter(core *framework.Core) {
+	// core.Get("foo", framework.TimeoutHandler(FooControllerHandler, time.Second*1))
+	core.Get("foo", FooControllerHandler)
 }
